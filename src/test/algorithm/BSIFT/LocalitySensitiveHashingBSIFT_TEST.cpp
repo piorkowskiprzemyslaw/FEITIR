ac51e362fa4595a4d4b726455b3e580bebd0cfab
Przemek <piorkowskiprzemyslaw@gmail.com>
2016-08-23 19:47:40 +0200

    LSH test repaired

--- src/test/algorithm/BSIFT/LocalitySensitiveHashingBSIFT_TEST.cpp
+++ src/test/algorithm/BSIFT/LocalitySensitiveHashingBSIFT_TEST.cpp
@@ -34,15 +34,28 @@ BOOST_FIXTURE_TEST_SUITE(LocalitySensitiveHashingBSIFTExtractor_TEST, LocalitySe
 
     BOOST_AUTO_TEST_CASE(Database)
     {
-        BOOST_REQUIRE(true);
         auto hashFunctions = LocalitySensitiveHashingBSIFTExtractor::generateRandomHashFunctions(N);
-        BOOST_REQUIRE(true);
         LocalitySensitiveHashingBSIFTExtractor extractor(N, hashFunctions);
-        BOOST_REQUIRE(true);
+
         auto database = databaseFactory.createDatabase(resourcePath + imagePath);
         BOOST_REQUIRE(database != nullptr);
         auto transformedDatabase = extractor.extractDatabaseBSIFT(database);
     }
 
+    BOOST_AUTO_TEST_CASE(SingleImage)
+    {
+        auto hashFunctions = LocalitySensitiveHashingBSIFTExtractor::generateRandomHashFunctions(N);
+        LocalitySensitiveHashingBSIFTExtractor extractor(N, hashFunctions);
+
+        auto img = imageFactory.createImage(resourcePath + imagePath + lennaImage);
+        BOOST_REQUIRE(img != nullptr);
+        auto bsiftImg = extractor.extractImageBSIFT(img);
+        BOOST_REQUIRE(bsiftImg != nullptr);
+        for (auto bsift : bsiftImg->getBsift()) {
+            BOOST_REQUIRE(bsift.size() == N);
+            BOOST_REQUIRE(bsift.count() > 0);
+            BOOST_REQUIRE(bsift.count() < N);
+        }
+    }
 
 BOOST_AUTO_TEST_SUITE_END()
