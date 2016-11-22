6fd3ab6f2e6ac2178813dc775c125450f4af3179
Przemyslaw Piorkowski <piorkowskiprzemyslaw@gmail.com>
2016-11-22 22:17:10 +0100

    [Travis] Installing eigen corrected

--- tools/travis/install_eigen.sh
+++ tools/travis/install_eigen.sh
@@ -3,17 +3,20 @@
 EIGEN_VERSION=3.2.9
 EIGEN_HASH=dc6cfdf9bcec
 
-cd "$build_dir"
+ORIG_DIR=`pwd`
+
+cd ~
 wget --quiet "http://bitbucket.org/eigen/eigen/get/${EIGEN_VERSION}.tar.gz"
 tar xzf ${EIGEN_VERSION}.tar.gz
-cd "$build_dir/eigen-eigen-${EIGEN_HASH}/"
-mkdir -p "$build_dir/eigen-eigen-${EIGEN_HASH}/_build"
-cd "$build_dir/eigen-eigen-${EIGEN_HASH}/_build"
+cd "~/eigen-eigen-${EIGEN_HASH}/"
+mkdir -p "~/eigen-eigen-${EIGEN_HASH}/_build"
+cd "~/eigen-eigen-${EIGEN_HASH}/_build"
 
 # Build, make and install Eigen
 cmake .. -DCMAKE_INSTALL_PREFIX:STRING="$install_dir"
 make
 make install
+cd ${ORIG_DIR}
 
 # Check install
 pkg-config --modversion "eigen3 >= ${EIGEN_VERSION}"
